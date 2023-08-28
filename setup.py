6cb50c774fa8f14bfbd0007a33725993362c8a69
santiago123x <santiago123x@users.noreply.github.com>
2023-08-28 12:49:25 +0000

    Update version to v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aclimate_resampling",
-    version='v0.0.3',
+    version='v0.0.4',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="Resampling module",
