7b08c29b5a0555f59d2637ac14d060057ec930d9
mariavictoriadiaz <mariavictoriadiaz@users.noreply.github.com>
2024-01-30 19:22:52 +0000

    Update version to v0.0.37

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aclimate_resampling",
-    version='v0.0.36',
+    version='v0.0.37',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="Resampling module",
