8259d014dc344757c6c8028cf1aa7dc79e202519
santiagoC <s.calderon@cgiar.org>
2024-10-08 14:27:38 -0500

    setup 3.9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.9",
     entry_points={
         'console_scripts': [
             'aclimate_resampling=aclimate_resampling.aclimate_run_resampling:main',
